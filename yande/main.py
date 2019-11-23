f50b802eb0f3cb4ad936379d3dc329c4294c266a
ice-b-114 <57836568+ice-b-114@users.noreply.github.com>
2019-11-23 12:03:40 +0800

    Add files via upload

--- yande/main.py
+++ yande/main.py
@@ -8,16 +8,23 @@ def download(img_url,pname):
     #print(r.status_code) # 返回状态码
     if r.status_code == 200:
         open(pname+'.jpg', 'wb').write(r.content) # 将内容写入图片
-        print("Done")
+        print("Done\n")
     else:
         print("Fail")
 def main(limit,*tag):
     tags = '+'.join(tag)
     res=requests.get(url='http://yande.re/post.xml?limit='+str(limit)+'&tags='+tags)
-    print(res.text)
     pic_address = re.findall(r'jpeg_url="(.+?)"', res.text, )
     pic_name = re.findall(r'<post id="(.+?)"', res.text, )
+    pic_tag = re.findall(r'tags="(.+?)"', res.text, )
+    pic_author = re.findall(r'author="(.+?)"', res.text, )
     for i in range(0,limit):
+        print('ID:'+pic_name[i])
+        print('Author:'+pic_author[i])
+        print('tags:'+pic_tag[i])
         download(pic_address[i],pic_name[i])
 if __name__ == '__main__':
-    main(1,"order%3Arandom")#order%3Arandom
+    while True:
+        num = input('Download number:')
+        assert num.isnumeric()
+        main(int(num),"order%3Arandom")#order%3Arandom
