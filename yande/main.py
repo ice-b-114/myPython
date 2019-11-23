e3dc86fb3b5c2b7067cfb0fd16558fcf1242f0e8
ice-b-114 <57836568+ice-b-114@users.noreply.github.com>
2019-11-23 17:28:49 +0800

    Add files via upload
    自动下

--- yande/main.py
+++ yande/main.py
@@ -14,17 +14,17 @@ def download(img_url,pname):
 def main(limit,*tag):
     tags = '+'.join(tag)
     res=requests.get(url='http://yande.re/post.xml?limit='+str(limit)+'&tags='+tags)
-    pic_address = re.findall(r'jpeg_url="(.+?)"', res.text, )
-    pic_name = re.findall(r'<post id="(.+?)"', res.text, )
-    pic_tag = re.findall(r'tags="(.+?)"', res.text, )
-    pic_author = re.findall(r'author="(.+?)"', res.text, )
+    pic_address = re.findall(r'jpeg_url="(.*?)"', res.text, )
+    pic_name = re.findall(r'<post id="(.*?)"', res.text, )
+    pic_tag = re.findall(r'tags="(.*?)"', res.text, )
+    pic_author = re.findall(r'source="(.*?)"', res.text, )
+#    print(res.text)
     for i in range(0,limit):
         print('ID:'+pic_name[i])
-        print('Author:'+pic_author[i])
+        print('source:'+pic_author[i])
         print('tags:'+pic_tag[i])
         download(pic_address[i],pic_name[i])
+        with open('info.txt', "a+",encoding='utf-8-sig') as f:
+            f.write('ID:{}\nsource:{}\ntags:{}\n'.format(pic_name[i],pic_author[i],pic_tag[i]))
 if __name__ == '__main__':
-    while True:
-        num = input('Download number:')
-        assert num.isnumeric()
-        main(int(num),"order%3Arandom")#order%3Arandom
+    main(5,"order%3Arandom+breasts")#order%3Arandom
